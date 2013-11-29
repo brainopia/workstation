301791509e895695a23ce6c4edfde3155af9ae11
brainopia <brainopia@evilmartians.com>
2013-11-29 13:50:10 +0400

    Add vim and terminal to default run

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -3,10 +3,12 @@
   rubies
   main::usability
   main::packages
+  main::vim
   main::git
   main::chef
   main::ssh
   main::applications
+  main::terminal
 ]
 .each do |name|
   include_recipe name 
