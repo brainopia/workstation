9c33bbe3acdb4ee8ee7a2dce113cb41e22224216
brainopia <brainopia@evilmartians.com>
2013-12-25 15:38:59 +0400

    Use more verbose boot screen

--- cookbooks/main/recipes/optimization.rb
+++ cookbooks/main/recipes/optimization.rb
@@ -6,3 +6,13 @@ file '/etc/sysctl.d/60-optimization.conf' do
 end
 
 execute 'sysctl -p /etc/sysctl.d/60-optimization.conf'
+
+ruby_block 'edit grub' do
+  block do
+    grub = Chef::Util::FileEdit.new '/etc/default/grub'
+    grub.search_file_delete_line /GRUB_CMDLINE_LINUX_DEFAULT/
+    grub.write_file
+  end
+end
+
+execute 'update-grub'
