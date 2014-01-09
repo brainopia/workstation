0c7e1fd3b55cae49d9e80914c9bb90fcd6cdcd20
brainopia <brainopia@evilmartians.com>
2014-01-09 23:01:00 +0400

    Add pry related gems

--- cookbooks/main/recipes/ruby.rb
+++ cookbooks/main/recipes/ruby.rb
@@ -11,7 +11,10 @@ file "/home/#{node[:user]}/.irbrc" do
 end
 
 file "/home/#{node[:user]}/.pryrc" do
-  content 'Pry.config.prompt = Pry::SIMPLE_PROMPT'
+  content <<-CODE
+    require_relative 'code/ruby/debundle'
+    Pry.config.prompt = Pry::SIMPLE_PROMPT
+  CODE
   owner node[:user]
   group node[:user]
 end
@@ -25,8 +28,27 @@ file "/home/#{node[:user]}/.bundle/config" do
   content <<-END.gsub(/^ {4}/, '')
     ---
     BUNDLE_PATH: .bundle
-    BUNDLE_DISABLE_SHARED_GEMS: '1'
   END
   owner node[:user]
   group node[:user]
+end
+
+remote_file "/home/#{node[:user]}/code/ruby/debundle.rb" do
+  source 'https://raw.github.com/ConradIrwin/pry-debundle/master/lib/pry-debundle.rb'
+  owner node[:user]
+  group node[:user]
+end
+
+bash 'install gems' do
+  user node[:user]
+  flags '--login'
+  code <<-SH
+    source /usr/local/share/chruby/chruby.sh
+    chruby ruby
+    gem install pry-doc pry-docmore pry-editline \
+                pry-byebug awesome_print \
+                pry-pretty-numeric bond \
+                pry-rescue pry-stack_explorer \
+                pry-remote pry-vterm_aliases
+  SH
 end
