08611a59294af1db1f4d7214538b21f029759c69
brainopia <brainopia@evilmartians.com>
2014-01-13 13:18:05 +0400

    Add pry-syntax-hacks

--- cookbooks/main/recipes/ruby.rb
+++ cookbooks/main/recipes/ruby.rb
@@ -49,6 +49,7 @@ bash 'install gems' do
                 pry-byebug awesome_print \
                 pry-pretty-numeric bond \
                 pry-rescue pry-stack_explorer \
-                pry-remote pry-vterm_aliases
+                pry-remote pry-vterm_aliases \
+                pry-syntax-hacks
   SH
 end
