e3e45ac341ed2bc4ee73e55fb87547b28c8d6259
brainopia <brainopia@evilmartians.com>
2014-01-20 21:14:15 +0400

    Latest updates

--- cookbooks/main/recipes/vim.rb
+++ cookbooks/main/recipes/vim.rb
@@ -5,37 +5,49 @@ template '/etc/vim/vimrc.local' do
 end
 
 %w[
-  autoload
-  bundle
-].each do |name|
-  directory "/home/#{node[:user]}/.vim/#{name}" do
+  ~/.vim/autoload
+  ~/.vim/bundle
+  ~/.fonts
+  ~/.config/fontconfig/conf.d
+].each do |path|
+  path.sub! '~', "/home/#{node[:user]}"
+  directory path do
     user node[:user]
     group node[:user]
     recursive true
   end
 end
 
-remote_file "/home/#{node[:user]}/.vim/autoload/pathogen.vim" do
-  source 'https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim'
-  owner node[:user]
-  group node[:user]
-  action :create_if_missing
+{
+  '~/.vim/autoload' =>
+  'https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim',
+
+  '~/.fonts' =>
+  'https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf',
+
+  '~/.config/fontconfig/conf.d' =>
+  'https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf'
+}.each do |path, from|
+  path = path.sub '~', "/home/#{node[:user]}"
+  path += '/' + from.split('/').last
+  remote_file path do
+    source from
+    owner node[:user]
+    group node[:user]
+  end
 end
 
-git "/home/#{node[:user]}/.vim/bundle/nerdtree" do
-  repository 'https://github.com/scrooloose/nerdtree.git'
-  user node[:user]
-  group node[:user]
-end
-
-git "/home/#{node[:user]}/.vim/bundle/ctrlp" do
-  repository 'https://github.com/kien/ctrlp.vim.git'
-  user node[:user]
-  group node[:user]
-end
-
-git "/home/#{node[:user]}/.vim/bundle/vim-slim" do
-  repository 'https://github.com/slim-template/vim-slim.git'
-  user node[:user]
-  group node[:user]
+%w[
+  scrooloose/nerdtree
+  kien/ctrlp.vim
+  slim-template/vim-slim
+  bling/vim-airline
+  altercation/vim-colors-solarized
+].each do |repo|
+  name = repo.split('/').last
+  git "/home/#{node[:user]}/.vim/bundle/#{name}" do
+    repository "https://github.com/#{repo}.git"
+    user node[:user]
+    group node[:user]
+  end
 end
