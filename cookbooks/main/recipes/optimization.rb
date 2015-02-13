b9b6e90c6fa36f6028bbb74535bce6097fb3c9a9
brainopia <brainopia@evilmartians.com>
2015-02-13 14:39:27 +0300

    Drop grub customization

--- cookbooks/main/recipes/optimization.rb
+++ cookbooks/main/recipes/optimization.rb
@@ -2,19 +2,9 @@ package 'zram-config'
 
 file '/etc/sysctl.d/60-optimization.conf' do
   content <<-END.gsub(/^ {4}/, '')
-    vm.swappiness = 20
+    vm.swappiness = 40
     vm.dirty_writeback_centisecs = 3000
   END
 end
 
 execute 'sysctl -p /etc/sysctl.d/60-optimization.conf'
-
-ruby_block 'edit grub' do
-  block do
-    grub = Chef::Util::FileEdit.new '/etc/default/grub'
-    grub.search_file_delete_line /GRUB_CMDLINE_LINUX_DEFAULT/
-    grub.write_file
-  end
-end
-
-execute 'update-grub'
