4e08c6a02c379936e7d16ec589c7a03a014b445d
brainopia <brainopia@evilmartians.com>
2015-02-14 15:13:22 +0300

    Persist touchpad changes

--- cookbooks/main/recipes/usability.rb
+++ cookbooks/main/recipes/usability.rb
@@ -1,27 +1,3 @@
-synclient = {
-  TapButton1: 1,          # touchpad tap
-  TapButton2: 3,
-  TapButton3: 2,
-  VertScrollDelta: -77,   # natural scrolling
-  HorizScrollDelta: -77,
-  FingerHigh: 3,          # less sensitive tap
-  TapAndDragGesture: 0,   # disable tap and drag
-  PressureMotionMinZ: 40, # less sensitive motion
-  PalmDetect: 1           # detect palm
-}
-
-file "/home/#{node[:user]}/.xinitrc" do
-  action :create
-  user node[:user]
-  group node[:user]
-  content synclient.map {|key, value| "synclient #{key}=#{value}" }.join("\n")
-end
-
-execute 'apply synclient' do
-  command "bash /home/#{node[:user]}/.xinitrc"
-end
-
-
 def gsetting(scheme, key, value)
   bash key do
     code "gsettings set #{scheme} #{key} '#{value}'"
@@ -30,26 +6,16 @@ def gsetting(scheme, key, value)
   end
 end
 
+
+# disable gnome touchpad settings
+gsetting 'org.gnome.settings-daemon.plugins.mouse', 'active', false
+
+# setup touchpad
+template '/etc/X11/xorg.conf.d/50-synaptics.conf' do
+  source 'synaptics.conf'
+  user node[:user]
+  group node[:user]
+end
+
+# increase text size
 gsetting 'org.gnome.desktop.interface', 'text-scaling-factor' , 1.4
-
-package 'wmctrl'
-
-shortcuts = [
-  ['Skype', 'sh -c "wmctrl -xa skype.Skype || PULSE_LATENCY_MSEC=30 skype"', '<Alt>s'],
-  ['Chrome', 'sh -c "wmctrl -xa google-chrome.Google-chrome || google-chrome"', '<Alt>d'],
-  ['Terminator', 'sh -c "wmctrl -xa terminator.Terminator || terminator"', '<Alt>f']
-]
-
-paths = shortcuts.size.times.map do |i|
-  "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom#{i}/"
-end
-
-gsetting 'org.gnome.settings-daemon.plugins.media-keys', 'custom-keybindings', paths
-
-paths.each.with_index do |path, i|
-  shortcut = shortcuts[i]
-  scheme = "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:#{path}"
-  gsetting scheme, 'name',    shortcut[0]
-  gsetting scheme, 'command', shortcut[1]
-  gsetting scheme, 'binding', shortcut[2]
-end
