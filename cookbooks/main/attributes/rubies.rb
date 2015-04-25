b089a54b9b8d3c3eb4a2ef0282ca78ccb2718bba
brainopia <brainopia@evilmartians.com>
2015-04-26 02:57:19 +0300

    Update ruby

--- cookbooks/main/attributes/rubies.rb
+++ cookbooks/main/attributes/rubies.rb
@@ -1,7 +1,7 @@
 normal.chruby_install.auto_upgrade = true
 normal.chruby_install.use_embedded = true
 normal.chruby_install.auto = true
-normal.chruby_install.default_ruby = 'ruby-2.2.0'
+normal.chruby_install.default_ruby = 'ruby-2.2.2'
 
-normal.rubies.list = ['ruby 2.2.0']
+normal.rubies.list = ['ruby 2.2.2']
 normal.rubies.install_bundler = true
